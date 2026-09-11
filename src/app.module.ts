c23a90434a96c3bca4e93d88837cd69fe0b3f11e
Victor Tavares <vte80988@lasallebajio.edu.mx>
2026-09-11 14:50:56 -0600

    auth: Implementación de registro, login y bcrypt

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,29 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { envs } from './config/envs';
 import { ReportsModule } from './reports/reports.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { EmailModule } from './email/email.module';
 
 @Module({
-  imports: [ReportsModule, UsersModule, AuthModule, EmailModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: envs.DB_HOST,
+      port: envs.DB_PORT,
+      username: envs.DB_USER,
+      password: envs.DB_PASSWORD,
+      database: envs.DB_NAME,
+      synchronize: false,
+      autoLoadEntities: true,
+    }),
+    ReportsModule,
+    UsersModule,
+    AuthModule,
+    EmailModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,19 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('SYSTEM_USER')
+export class User {
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column()
+    name!: string;
+
+    @Column()
+    email!: string;
+
+    @Column()
+    password!: string;
+
+    @Column({ default: true })
+    isNotificationEnabled!: boolean;
+}
